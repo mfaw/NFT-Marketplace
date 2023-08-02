5245cf3af965cd09ab20803d5726a4c2606e7677
mfaw <mfawzii96@gmail.com>
2023-08-01 21:23:31 -0800

    "Removed node_modules folder"

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract NFT{
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721{
+    // a constructor requires a name and a symbol of the ERC721 token
+    constructor() ERC721("My cool NFT", "NFT") {}
+
+    uint private _tokenId = 0;
+
+    // must have a mint function
+    function mint() external returns (uint){
+        _tokenId++;
+        // the NFT is minted to the person calling this function
+        _mint(msg.sender, _tokenId);
+        return _tokenId;
+    }
+
 }
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+const Market = artifacts.require("Market");
+const NFT = artifacts.require("NFT");
+
+module.exports = function (deployer) {
+    deployer.deploy(NFT);
+    deployer.deploy(Market);
+};
